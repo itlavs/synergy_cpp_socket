3c6db0d4f546cbde41d17f45868d5176e17ecfa4
КК-Семен-309-Препод <edu-msk3-309t@synergy.local>
2020-10-06 22:09:04 +0300

    create, bind and link socket

--- Socket/Socket.cpp
+++ Socket/Socket.cpp
@@ -9,12 +9,12 @@
 #include "stdafx.h"
 #include "winsock.h"
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-	WSADATA WsaData;
-	char chInfo[64];
+WSADATA WsaData;
+SOCKET servsocket;
 
-	// Подключение к сокетам
+// Подключение к сокетам
+int StartWinSock()
+{
 	int err = WSAStartup(0x0101, &WsaData);
 	if (err == SOCKET_ERROR)
 	{
@@ -24,9 +24,15 @@ int _tmain(int argc, _TCHAR* argv[])
 	else 
 	{
 		printf("Winsock init OK\n");
+		return 0;
 	}
+}
 
-	// Получение имени машины
+// Получение имени машины
+void GetHostName()
+{
+	char chInfo[64];
+
 	if (gethostname(chInfo, sizeof(chInfo)))
 	{
 		printf("Not local host\n");
@@ -35,8 +41,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	{
 		printf("Hostname: %s\n", chInfo);
 	}
+}
 
-	// Очистка
+// Очистка сокетов
+void CleanWinSock()
+{
 	if (WSACleanup())
 	{
 		printf("Error cleanup\n");
@@ -46,6 +55,68 @@ int _tmain(int argc, _TCHAR* argv[])
 		printf("Cleanup OK\n");
 	}
 	system("pause");
+}
+
+// Создание сокета
+void CreateSocket()
+{
+	servsocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (servsocket == INVALID_SOCKET)
+	{
+		printf("Error on create socket\n");
+		exit(1);
+	}
+	else
+	{
+		printf("Socket OK\n");
+	}
+}
+
+void BindSocket()
+{
+	SOCKADDR_IN socketaddress;
+	socketaddress.sin_family = AF_INET;
+	socketaddress.sin_addr.s_addr = INADDR_ANY;
+	socketaddress.sin_port = 80;
+
+	if (bind(servsocket, (LPSOCKADDR)&socketaddress, sizeof(socketaddress)) == SOCKET_ERROR)
+	{
+		printf("Error bind socket\n");
+		//exit(1);
+	}
+	else
+	{
+		printf("Bind socket OK\n");
+	}
+}
+
+void WaitSocket()
+{
+	#define QUEUE_SIZE 5
+	if (listen(servsocket, QUEUE_SIZE) == SOCKET_ERROR)
+	{
+		printf("Error listen socket\n");
+		//exit(1);
+	}
+	else
+	{
+		printf("Wait for connection\n");
+	}
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	if (StartWinSock())
+	{
+		return 1;
+	}
+	GetHostName();
+	CreateSocket();
+	BindSocket();
+	WaitSocket();
+	closesocket(servsocket);
+	CleanWinSock();
+
 	return 0;
 }
 
